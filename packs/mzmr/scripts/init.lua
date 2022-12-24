a272e419360f5f2f39b0970b6b4d9c150c6a40b2
Gekiryuu86 <jikannotorento@gmail.com>
2022-12-24 01:51:25 -0700

    Removed references to bingo on master.
    Moved to branch dev/mzmr/bingo

--- packs/mzmr/scripts/init.lua
+++ packs/mzmr/scripts/init.lua
@@ -1,20 +1,14 @@
 Tracker:AddItems("items/bosses.json")
 Tracker:AddItems("items/unique.json")
-Tracker:AddItems("items/bingo.json")
 if (string.find(Tracker.ActiveVariantUID, "hard")) then
     Tracker:AddItems("items/tanks_hard.json")
   else
     Tracker:AddItems("items/tanks_normal.json")
 end
 
-Tracker:AddLayouts("layouts/shared/bingo.json")
 Tracker:AddLayouts("layouts/shared/items.json")
 Tracker:AddLayouts("layouts/shared/bosses.json")
 
-if (string.find(Tracker.ActiveVariantUID, "bingo")) then
-    Tracker:AddLayouts("layouts/shared/layout_bingo.json")
-  else
-    Tracker:AddLayouts("layouts/shared/layout_default.json")
-end
+Tracker:AddLayouts("layouts/shared/layout_default.json")
 
 Tracker:AddLayouts("layouts/tracker.json")
